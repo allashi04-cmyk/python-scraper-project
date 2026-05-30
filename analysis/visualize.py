f58f96c15aacbbacf2671629b389774911ae70ff
allashi04-cmyk <allashi04@gmail.com>
2026-05-31 02:17:52 +0500

    Исправила вывод данных скрапера, теперь значения выводятся не по дням, а по динамически изменяющимся временным интервалам

--- analysis/visualize.py
+++ analysis/visualize.py
@@ -1,17 +1,20 @@
 import os
 import matplotlib
-matplotlib.use('Agg')  #  Важно: отключает GUI-окна в облачной среде
+matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 import pandas as pd
 
 def plot_price_trend(df: pd.DataFrame):
-    daily_avg = df.groupby(df["scraped_at"].dt.date)["price_num"].mean().reset_index()
-    daily_avg.columns = ["date", "avg_price"]
 
-    plt.figure(figsize=(8, 5))
-    plt.plot(daily_avg["date"], daily_avg["avg_price"], marker="o", linestyle="-", color="#1f77b4")
+     # Группировка по точному времени каждого запуска
+    daily_avg = df.groupby('scraped_at')['price_num'].mean().reset_index()
+    daily_avg.columns = ['datetime', 'avg_price']
+    
+    plt.figure(figsize=(10, 5))
+    plt.plot(daily_avg['datetime'], daily_avg['avg_price'], 
+             marker='o', linestyle='-', color='#1f77b4')
     plt.title("Динамика средней цены книг")
-    plt.xlabel("Дата запуска")
+    plt.xlabel("Время запуска")
     plt.ylabel("Средняя цена (£)")
     plt.grid(alpha=0.3)
     plt.xticks(rotation=45)
