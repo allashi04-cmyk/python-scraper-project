7103292a93594d9c47298020f6bf82dc3424f099
allashi04-cmyk <allashi04@gmail.com>
2026-06-26 20:23:54 +0500

    Добавила фильтры для параметра ЦЕНА, чтобы убрать валюту для дальнейшего приведения к float

--- api/dependecies.py
+++ api/dependecies.py
@@ -11,5 +11,19 @@ def load_books() -> pd.DataFrame:
         raise HTTPException(404, "Файл с данными не найден. Сначала запустите scraper.")
     df = pd.read_csv(CSV_PATH)
     df['scraped_at'] = pd.to_datetime(df['scraped_at'])
+
+    if 'price' in df.columns:
+        df['price'] = (
+            df['price']
+            .astype(str)
+            .str.replace('£', '', regex=False)
+            .str.replace('Â', '', regex=False)
+            .str.replace('$', '', regex=False)
+            .str.replace('€', '', regex=False)
+            .str.strip()
+        )
+        # Конвертируем в numeric
+        df['price'] = pd.to_numeric(df['price'], errors='coerce')
+
     # Оставляем только уникальные записи по последней метке времени (опционально)
     return df.dropna(subset=["price", "title"])
